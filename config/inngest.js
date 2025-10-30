3c841fda4bd9440b9b645584339a667787ebf1d3
philemon673 <fuadochris@gmail.com>
2025-10-30 05:38:20 +0100

    change the id to "quickcard-next"

--- config/inngest.js
+++ config/inngest.js
@@ -4,7 +4,7 @@ import User from "@/models/user";
 import Order from "@/models/order";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "quickcard-next"
+export const inngest = new Inngest({ id: "quickcart-next"
  });
 
 // inngest function to save user data to database
